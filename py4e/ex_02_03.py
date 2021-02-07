fbc2aff062ce58e4e1845b7857734d97abb3bde4
MimD-Jang <78602020+MimD-Jang@users.noreply.github.com>
2021-02-08 00:18:18 +0900

    Delete py4e directory

--- py4e/ex_02_03.py
+++
@@ -1,9 +0,0 @@
-'''print("py4E")
-우선 window+R로 cmd창부터 띄운 다음에
-디렉터리부터 cd를 이용해 이동 후
-맞게 py4e파일까지 위치했는지 확인해보기 위해
-print함수 사용해서 결과값 확인하기'''
-xh = input('Enter hours : ')
-xr = input('Enter rate : ')
-xp = float(xh)*float(xr)
-print('Pay is :',xp)
